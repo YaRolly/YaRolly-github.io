99a3f4bc202b3bd2a3941a5a54ff1401a3791af6
YaRolly <Ea4GoodRu@gmail.com>
2023-03-06 03:29:00 +0300

    test 30

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    target: 'esnext',
+    outDir: 'build',
+    assetsDir: '',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        format: 'cjs'
+      }
+    }
   }
 })
